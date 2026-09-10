53045b356f494d07873534df592f27e0f44cd700
oezballar <oezballar@prime-research.com>
2026-09-10 18:33:38 +0200

    löst flyway Problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencyManagement {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-flyway")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.modulith:spring-modulith-starter-core")
